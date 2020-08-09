d497fc8dbea0215dc7620e1db5d8c972f3edebbe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-09 07:50:27 +0000

    Deploy Chiranjibzlt/React-calipso to github.com/Chiranjibzlt/React-calipso.git:gh-pages

---
+++ precache-manifest.57917075a1380aded42aaf6c1f9e6d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc894b765ea5956ac656cf03c83fe12b",
+    "url": "/react-calipso/index.html"
+  },
+  {
+    "revision": "b4b9ba12085f2bdae11c",
+    "url": "/react-calipso/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b16bc3c8a6f0f3f5d3db",
+    "url": "/react-calipso/static/js/2.ede97b6c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-calipso/static/js/2.ede97b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b9ba12085f2bdae11c",
+    "url": "/react-calipso/static/js/main.3bab9013.chunk.js"
+  },
+  {
+    "revision": "5965a0d22a31c1a29e8e",
+    "url": "/react-calipso/static/js/runtime-main.8ca8bed6.js"
+  }
+]);
